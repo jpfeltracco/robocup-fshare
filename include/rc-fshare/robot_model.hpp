11b92e4962e2e707371b63a944a03f1de2eead76
Jeremy Feltracco <jpfeltracco@gatech.edu>
2018-05-30 21:53:43 -0400

    Updates to clean up robot model

--- include/rc-fshare/robot_model.hpp
+++ include/rc-fshare/robot_model.hpp
@@ -12,54 +12,59 @@
 /// Model parameters for a robot.  Used by the controls system.
 class RobotModel {
 
-static constexpr double DegreesToRadians(double val) { return val * M_PI / 180.0; }
-
-private:
-    RobotModel() {
-        WheelRadius = 0.02786;
-        WheelAngles = {
-            DegreesToRadians(180 - 30),  // M1
-            DegreesToRadians(180 + 39),  // M2
-            DegreesToRadians(360 - 39),  // M3
-            DegreesToRadians(0 + 30),    // M4
-        };
-
-        WheelDist = 0.0798576;
-
-        recalculateBotToWheel();
-    }
+static constexpr double DegToRad(double val) { return val * M_PI / 180.0; }
 
 public:
+    using EncReading = Eigen::Matrix<int32_t, 4, 1>;
+
     // singleton pattern
     static RobotModel& get() {
         static RobotModel instance; // Guaranteed to be destroyed.
         // Instantiated on first use.
         return instance;
     }
-
     RobotModel(RobotModel const&) = delete;
     void operator=(RobotModel const&) = delete;
 
-    /// Radius of omni-wheel (in meters)
+public:
+    // Wheel angles (in radians) measured between +x axis and wheel axle
+    std::array<double, 4> WheelAngles;
+    // Radius of omni-wheel (in meters)
     double WheelRadius;
-
-    /// Distance from center of robot to center of wheel
+    // Distance from center of robot to center of wheel
     double WheelDist;
 
-    /// Wheel angles (in radians) measured between +x axis and wheel axle
-    std::array<double, 4> WheelAngles;
+    // Made up lower level motor constant
+    // duty_cycle * multiplier == motor vel
+    double DutyCycleMultiplier;
 
-    /// wheelSpeeds = BotToWheel * V_bot
+    // Number of encoder increments for every full revolution
+    int32_t EncTicksPerRev;
+
+    // wheelSpeeds = BotToWheel * V_bot
     Eigen::Matrix<double, 4, 3> BotToWheel;
+    // botSpeed = WheelToBot * wheelSpeeds
     Eigen::Matrix<double, 3, 4> WheelToBot;
+    // botSpeed = EncToBot * EncoderReading
+    Eigen::Matrix<double, 3, 4> EncToBot;
 
-    /// This should be called when any of the other parameters are changed
-    void recalculateBotToWheel() {
-        // See this paper for more info on how this matrix is derived:
+private:
+    RobotModel()
+    {
+        WheelAngles = {
+            DegToRad(180 - 30),  // M1
+            DegToRad(180 + 39),  // M2
+            DegToRad(360 - 39),  // M3
+            DegToRad(0 + 30),    // M4
+        };
+
+        WheelRadius = 0.02786;
+        WheelDist = 0.0798576;
+        EncTicksPerRev = 2048 * 3;
+
+        DutyCycleMultiplier = 2.0;
+
         // http://people.idsia.ch/~foerster/2006/1/omnidrive_kiart_preprint.pdf
-
-        // Factor WheelDist (R) into this matrix
-        // clang-format off
         BotToWheel <<
             -sinf(WheelAngles[0]), cosf(WheelAngles[0]), WheelDist,
             -sinf(WheelAngles[1]), cosf(WheelAngles[1]), WheelDist,
@@ -68,12 +73,10 @@ public:
         // Invert because our wheels spin opposite to paper
         BotToWheel *= -1;
         BotToWheel /= WheelRadius;
-        // clang-format on
+
         WheelToBot = (BotToWheel.transpose() * BotToWheel).inverse() * BotToWheel.transpose();
+
+        EncToBot = WheelToBot * 2.0*M_PI / EncTicksPerRev;
     }
 
-    float DutyCycleMultiplier = 2.0f;
 };
-
-/// Model parameters for robot.  See RobotModel.cpp for values.
-extern const RobotModel RobotModelControl;
